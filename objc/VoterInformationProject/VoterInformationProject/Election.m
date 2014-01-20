058207aa20541eb13ad1ddc862b3c620c228e6b9
CloudNiner <andrew.fink.11@gmail.com>
2014-01-20 18:51:23 -0500

    Add stub Election class

---
+++ objc/VoterInformationProject/VoterInformationProject/Election.m
@@ -0,0 +1,33 @@
+//
+//  Election.m
+//  VoterInformationProject
+//
+//  Created by Andrew Fink on 1/20/14.
+//  Copyright (c) 2014 Bennet Huber. All rights reserved.
+//
+
+#import "Election.h"
+
+@implementation Election
+
+- (id) initWithId:(NSString *)electionID {
+    return [self initWithId:electionID
+                    andName:@""
+                    andDate:@"1970-01-01"];
+}
+
+- (id) initWithId:(NSString *)electionID andName:(NSString *)name andDate:(NSString *)date {
+    self = [super init];
+    if (self) {
+        self.electionId = electionID;
+        self.name = name;
+        self.date = date;
+    }
+    return self;
+}
+
+- (BOOL) isInactive {
+    return NO;
+}
+
+@end
