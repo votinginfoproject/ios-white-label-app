caa4062574305308e02cb46d7f7845d4f10bcea3
CloudNiner <andrew.fink.11@gmail.com>
2014-01-20 16:39:24 -0500

    Add skeleton app views

---
+++ objc/VoterInformationProject/VoterInformationProject/FindElectionsViewController.h
@@ -0,0 +1,12 @@
+//
+//  FindElectionsViewController.h
+//  VoterInformationProject
+//
+//  Created by Andrew Fink on 1/20/14.
+//  Copyright (c) 2014 Bennet Huber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FindElectionsViewController : UITableViewController
+@end
