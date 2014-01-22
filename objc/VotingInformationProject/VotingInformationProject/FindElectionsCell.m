6fa95b079b3de2adbcb31d5be5668a4fa57d10c8
CloudNiner <andrew.fink.11@gmail.com>
2014-01-22 15:05:59 -0500

    Clean up FindElections table view with custom cell

---
+++ objc/VotingInformationProject/VotingInformationProject/FindElectionsCell.m
@@ -0,0 +1,29 @@
+//
+//  FindElectionsCell.m
+//  VotingInformationProject
+//
+//  Created by Andrew Fink on 1/22/14.
+//  Copyright (c) 2014 Bennet Huber. All rights reserved.
+//
+
+#import "FindElectionsCell.h"
+
+@implementation FindElectionsCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
