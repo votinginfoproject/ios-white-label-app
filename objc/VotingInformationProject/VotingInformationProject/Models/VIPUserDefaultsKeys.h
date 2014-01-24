4bcec64d71b871701fd330a0c7d1625c98121a12
CloudNiner <andrew.fink.11@gmail.com>
2014-01-24 15:09:05 -0500

    Remove unnecessary Foundation import

--- objc/VotingInformationProject/VotingInformationProject/Models/VIPUserDefaultsKeys.h
+++ objc/VotingInformationProject/VotingInformationProject/Models/VIPUserDefaultsKeys.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2014 Bennet Huber. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
 #define USER_DEFAULTS_STORED_ADDRESS @"StoredAddress"
 #define USER_DEFAULTS_LATITUDE_KEY @"mapLatitude"
 #define USER_DEFAULTS_LONGITUDE_KEY @"mapLongitude"
