4103dcb761148f11a9b761bdd7de947a72443dc7
CloudNiner <andrew.fink.11@gmail.com>
2014-02-12 13:22:50 -0500

    Remove all existing copyright refs from project

--- objc/VotingInformationProject/VotingInformationProject/Controllers/BallotDetailsViewController.h
+++ objc/VotingInformationProject/VotingInformationProject/Controllers/BallotDetailsViewController.h
@@ -3,7 +3,7 @@
 //  VotingInformationProject
 //
 //  Created by Andrew Fink on 2/7/14.
-//  Copyright (c) 2014 Bennet Huber. All rights reserved.
+//  
 //
 
 #import <UIKit/UIKit.h>
--- objc/VotingInformationProject/VotingInformationProject/CoreDataModels/ElectionAdministrationBody.m
+++ objc/VotingInformationProject/VotingInformationProject/CoreDataModels/ElectionAdministrationBody.m
@@ -3,7 +3,7 @@
 //  VotingInformationProject
 //
 //  Created by Andrew Fink on 2/6/14.
-//  Copyright (c) 2014 Bennet Huber. All rights reserved.
+//  
 //
 
 #import "ElectionAdministrationBody.h"
