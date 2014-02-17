143e7e12cb0524a1239562c3092c2e850e95ffcf
Bennet Huber <bhuber@azavea.com>
2014-02-17 10:32:40 -0500

    Remove copyright notices in test folder

--- objc/VotingInformationProject/VotingInformationProjectTests/ContactsSearchViewControllerTests.m
+++ objc/VotingInformationProject/VotingInformationProjectTests/ContactsSearchViewControllerTests.m
@@ -3,7 +3,6 @@
 //  VotingInformationProject
 //
 //  Created by Andrew Fink on 1/23/14.
-//  Copyright (c) 2014 Bennet Huber. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
--- objc/VotingInformationProject/VotingInformationProjectTests/ElectionAPITests.m
+++ objc/VotingInformationProject/VotingInformationProjectTests/ElectionAPITests.m
@@ -3,7 +3,6 @@
 //  VotingInformationProject
 //
 //  Created by Andrew Fink on 2/5/14.
-//  Copyright (c) 2014 Bennet Huber. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
