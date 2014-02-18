d8cfeb4625399d4f57b3be996762a012b5b9f898
CloudNiner <andrew.fink.11@gmail.com>
2014-02-18 16:28:44 -0500

    Add unit tests for Candidate/SocialChannel models

---
+++ objc/VotingInformationProject/VotingInformationProjectTests/SocialChannelTests.m
@@ -0,0 +1,38 @@
+//
+//  SocialChannelTests.m
+//  VotingInformationProject
+//
+//  Created by Andrew Fink on 2/3/14.
+//
+
+#import <XCTest/XCTest.h>
+#import "Kiwi.h"
+
+#import "SocialChannel+API.h"
+
+SPEC_BEGIN(SocialChannelTests)
+
+describe(@"SocialChannelTests", ^{
+    
+    beforeEach(^{
+        [MagicalRecord setupCoreDataStackWithInMemoryStore];
+    });
+    
+    afterEach(^{
+        [MagicalRecord cleanUp];
+    });
+
+    it(@"should ensure that uniqueUrl returns valid urls", ^{
+        SocialChannel *channel = (SocialChannel*)[SocialChannel setFromDictionary:@{@"id": @"VotingInfo", @"type": @"Facebook"}];
+        [[[[channel uniqueUrl] absoluteString] should] equal:@"https://facebook.com/VotingInfo"];
+
+        channel.type = @"Twitter";
+        [[[[channel uniqueUrl] absoluteString] should] equal:@"https://twitter.com/VotingInfo"];
+
+        channel.type = @"BadSocialMediaPlatform";
+        [[[channel uniqueUrl] should] beNil];
+    });
+   
+});
+
+SPEC_END
