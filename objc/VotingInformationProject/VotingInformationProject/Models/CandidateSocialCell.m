c9fb07e4af510003b492726b3b58a4e5bc41b2fa
CloudNiner <andrew.fink.11@gmail.com>
2014-02-19 15:59:56 -0500

    Refactor SocialChannel+API to include logo property, remove from SocialCell

--- objc/VotingInformationProject/VotingInformationProject/Models/CandidateSocialCell.m
+++ objc/VotingInformationProject/VotingInformationProject/Models/CandidateSocialCell.m
@@ -29,31 +29,8 @@
                        NSLocalizedString(@"View on %@", @"View on {social media type, i.e. Facebook/Twitter}"),
                        socialChannel.type];
     self.socialLabel.text = title;
-    self.url = [socialChannel uniqueUrl];
-    self.imageView.image = [self imageForType:socialChannel.type];
-}
-
-/**
- Get the proper social media logo as UIImage based on type
- *
- * @param type One of the string social media types returned by the votinginfoquery channels API
- *
- * @return UIImage* The image, or nil if no type matches
- */
-- (UIImage*)imageForType:(NSString*)type
-{
-    UIImage *image = nil;
-    NSString *lowerCaseType = [type lowercaseString];
-    if ([lowerCaseType isEqualToString:@"facebook"]) {
-        image = [UIImage imageNamed:@"facebook-logo-blue"];
-    } else if ([lowerCaseType isEqualToString:@"twitter"]) {
-        image = [UIImage imageNamed:@"twitter-logo-blue"];
-    } else if ([lowerCaseType isEqualToString:@"googleplus"]) {
-        image = [UIImage imageNamed:@"googleplus-logo-red"];
-    } else if ([lowerCaseType isEqualToString:@"youtube"]) {
-        image = [UIImage imageNamed:@"youtube-logo-red"];
-    }
-    return image;
+    self.url = [socialChannel url];
+    self.imageView.image = [socialChannel logo];
 }
 
 @end
