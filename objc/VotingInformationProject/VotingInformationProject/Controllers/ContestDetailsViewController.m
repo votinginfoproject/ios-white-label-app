d7caec8c82dd78f259f7052d7a7ed7dc3b531caf
CloudNiner <andrew.fink.11@gmail.com>
2014-02-19 14:22:04 -0500

    Final docs/cleanup for PR

--- objc/VotingInformationProject/VotingInformationProject/Controllers/ContestDetailsViewController.m
+++ objc/VotingInformationProject/VotingInformationProject/Controllers/ContestDetailsViewController.m
@@ -11,10 +11,6 @@
 #import "UIWebViewController.h"
 #import "ContestUrlCell.h"
 
-#define CDVC_TABLE_SECTION_PROPERTIES 0
-#define CDVC_TABLE_CELLID_PROPERTIES @"ContestPropertiesCell"
-#define CDVC_TABLE_SECTION_CANDIDATES 1
-#define CDVC_TABLE_CELLID_CANDIDATES @"CandidateCell"
 
 @interface ContestDetailsViewController ()
 
@@ -30,6 +26,12 @@
 @end
 
 @implementation ContestDetailsViewController
+
+NSString * const CDVC_TABLE_CELLID_PROPERTIES = @"ContestPropertiesCell";
+NSUInteger const CDVC_TABLE_SECTION_PROPERTIES = 0;
+NSString * const CDVC_TABLE_CELLID_CANDIDATES = @"CandidateCell";
+NSUInteger const CDVC_TABLE_SECTION_CANDIDATES = 1;
+NSString * const REFERENDUM_API_ID = @"Referendum";
 
 - (NSMutableArray*)tableData
 {
@@ -55,7 +57,8 @@
 }
 
 /**
- Refresh data displayed in the elements on this view
+ * Refresh data displayed in the elements on this view
+ * Conditionally display Referendum elements if self.contest.type == REFERENDUM_API_ID
  */
 - (void)updateUI
 {
@@ -67,7 +70,7 @@
     self.electionNameLabel.text = self.electionName ?: NSLocalizedString(@"Not Available", nil);
     [self.tableData addObject:[self.contest getContestProperties]];
 
-    if ([self.contest.type isEqualToString:@"Referendum"]) {
+    if ([self.contest.type isEqualToString:REFERENDUM_API_ID]) {
         self.contestNameLabel.text = self.contest.referendumTitle;
         self.title = NSLocalizedString(@"Referendum", nil);
     } else {
@@ -95,11 +98,13 @@
 {
     NSInteger section = [indexPath section];
     NSDictionary *property = (NSDictionary *)self.tableData[section][indexPath.item];
+    // Check if we can make a url from the data property
     NSURL *dataUrl = nil;
     if ([property isKindOfClass:[NSDictionary class]]) {
         dataUrl = [NSURL URLWithString:property[@"data"]];
     }
 
+    // If we have a url, make this cell segue to UIWebViewController
     if (section == CDVC_TABLE_SECTION_PROPERTIES && dataUrl.scheme && [dataUrl.scheme length] > 0) {
         ContestUrlCell *cell = (ContestUrlCell*)[tableView dequeueReusableCellWithIdentifier:CONTEST_URL_CELLID forIndexPath:indexPath];
         [self configureUrlTableViewCell:cell withDictionary:property];
@@ -120,7 +125,9 @@
 
 - (NSString*)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
-    if (section == CDVC_TABLE_SECTION_PROPERTIES) {
+    if (section == CDVC_TABLE_SECTION_PROPERTIES && [self.contest.type isEqualToString:REFERENDUM_API_ID]) {
+        return NSLocalizedString(@"Referendum Details", nil);
+    } else if (section == CDVC_TABLE_SECTION_PROPERTIES) {
         return NSLocalizedString(@"Contest Details", nil);
     } else if (section == CDVC_TABLE_SECTION_CANDIDATES) {
         return NSLocalizedString(@"Candidates", nil);
@@ -154,6 +161,12 @@
     cell.detailTextLabel.text = property[@"data"];
 }
 
+/**
+ *  Configure a ContestUrlCell
+ *
+ *  @param cell     ContestUrlCell to configure
+ *  @param property NSDictionary with keys title/data
+ */
 - (void)configureUrlTableViewCell:(ContestUrlCell*)cell
                                withDictionary:(NSDictionary*)property
 {
