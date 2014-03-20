117d94ff064d98d42f0eeefea9d37e37b43333db
Kathryn Killebrew <kkillebrew@azavea.com>
2014-03-20 16:25:24 -0400

    Open directions action sheet from polling location list

--- objc/VotingInformationProject/VotingInformationProject/Controllers/NearbyPollingViewController.h
+++ objc/VotingInformationProject/VotingInformationProject/Controllers/NearbyPollingViewController.h
@@ -22,4 +22,7 @@
  */
 - (IBAction)onViewSwitcherClicked:(id)sender;
 
+/** Open action sheet to prompt for getting directions from either map or list */
+- (void)openDirectionsActionSheet:(NSInteger)pollingLocationIndex;
+
 @end
