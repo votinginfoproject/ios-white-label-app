5a8a622b324113d48ba8034d61f360db602132aa
CloudNiner <andrew.fink.11@gmail.com>
2014-10-01 16:13:36 -0400

    Update Google Maps SDK to 1.8.1

---
+++ objc/VotingInformationProject/VotingInformationProject/Models/DirectionsViewSegueData.h
@@ -0,0 +1,13 @@
+//
+//  DirectionsViewSegueData.h
+//  VotingInformationProject
+//
+//  Created by Andrew Fink on 10/1/14.
+//  Copyright (c) 2014 The Pew Charitable Trusts. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DirectionsViewSegueData : NSObject
+
+@end
---
+++ objc/VotingInformationProject/VotingInformationProject/Models/DirectionsViewSegueData.m
@@ -0,0 +1,13 @@
+//
+//  DirectionsViewSegueData.m
+//  VotingInformationProject
+//
+//  Created by Andrew Fink on 10/1/14.
+//  Copyright (c) 2014 The Pew Charitable Trusts. All rights reserved.
+//
+
+#import "DirectionsViewSegueData.h"
+
+@implementation DirectionsViewSegueData
+
+@end
